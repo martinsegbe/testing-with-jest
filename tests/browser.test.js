0231c5ec1a0e66750fcafd119503012bca87cda9
martinsegbe <martinsegbe17@gmail.com>
2024-10-21 18:43:54 +0200

    Rättade felen som resulterade till fel i testningen

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -8,27 +8,29 @@ jest.setTimeout(1000 * 60 * 5); // 5 minuter
 
 // Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
 beforeAll(async () => {
-console.log(fileUnderTest);
+    console.log(fileUnderTest);
     driver = await new Builder().forBrowser('firefox').build();
     await driver.get(fileUnderTest);
-});
+}, 60000); // Timeout för beforeAll, sätter den till 60 sekunder
 
 // Allra sist avslutar vi Firefox igen
-afterAll(async() => {
-    await driver.quit();
-}, defaultTimeout);
+afterAll(async () => {
+    if (driver) {
+        await driver.quit();
+    }
+}, defaultTimeout); // Timeout för afterAll
 
 test('The stack should be empty in the beginning', async () => {
-	let stack = await driver.findElement(By.id('top_of_stack')).getText();
-	expect(stack).toEqual("n/a");
-});
+    let stack = await driver.findElement(By.id('top_of_stack')).getText();
+    expect(stack).toEqual("n/a");
+}, defaultTimeout); // Timeout för testet
 
 describe('Clicking "Pusha till stacken"', () => {
-	it('should open a prompt box', async () => {
-		let push = await driver.findElement(By.id('push'));
-		await push.click();
-		let alert = await driver.switchTo().alert();
-		await alert.sendKeys("Bananer");
-		await alert.accept();
-	});
+    it('should open a prompt box', async () => {
+        let push = await driver.findElement(By.id('push'));
+        await push.click();
+        let alert = await driver.switchTo().alert();
+        await alert.sendKeys("Bananer");
+        await alert.accept();
+    }, defaultTimeout); // Timeout för testet
 });
