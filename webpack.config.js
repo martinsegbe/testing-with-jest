a5ca6e970ea6dc36cd0edbfe675115d16ac023df
martinsegbe <128600133+martinsegbe@users.noreply.github.com>
2024-11-03 14:33:52 +0100

    Lade till Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{
+                context: __dirname + '/src/',
+                from: '*.html'
+            }]
+        })
+    ],
+    mode: 'development' // eller 'production' om du föredrar det
 };
-
